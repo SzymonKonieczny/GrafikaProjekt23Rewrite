82d80a729582c7579882b11ab8aa3ed9bc8ff658
SzymonKonieczny <kowladMC@gmail.com>
2023-05-23 17:21:04 +0200

    RedCarpet for the Maciek z Bogdanca

--- Crawler/Assets/WFC_Script.cs
+++ Crawler/Assets/WFC_Script.cs
@@ -6,36 +6,38 @@ namespace Crawler
 {
     enum RoomType
     {
-        ZWiseHall,
-        YWiseHall,
-        RoomCornerNW,
-        RoomCornerNE,
-        RoomCornerSW,
-        RoomCornerSE
+        Hall,
+        RoomCorner
     }
     struct Tile 
     {
-      
-        public   Vector2Int Position;
+
+        public Vector2Int Position;
         public RoomType Room;
+        public Quaternion Rotation;
+        void SetRotation(Vector3 Rot)
+        {
+            Rotation = Quaternion.Euler(Rot);
+        }
     }
 }
 public class WFC_Script : MonoBehaviour
 {
     [SerializeField] List<GameObject> RoomPrefabs = new List<GameObject>();
     [SerializeField] Dictionary<Vector2Int, GameObject> PlacedRooms = new Dictionary<Vector2Int, GameObject>();
-
+    
     private void PlaceRoom(Tile tile)
     {
         GameObject go = Instantiate(RoomPrefabs[(int)tile.Room]);
         go.transform.position = new Vector3(tile.Position.x, 0, tile.Position.y);
         PlacedRooms.Add(tile.Position, go);
+   
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
 
