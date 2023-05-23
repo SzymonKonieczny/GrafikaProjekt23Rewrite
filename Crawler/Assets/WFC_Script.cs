65c362fb7ce858a40e075501cfe0896afdff3ba4
SzymonKonieczny <kowladMC@gmail.com>
2023-05-23 16:28:08 +0200

    minor changes

---
+++ Crawler/Assets/WFC_Script.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crawler;
+namespace Crawler
+{
+    enum RoomType
+    {
+        ZWiseHall,
+        YWiseHall,
+        RoomCornerNW,
+        RoomCornerNE,
+        RoomCornerSW,
+        RoomCornerSE
+    }
+    struct Tile 
+    {
+      
+        public   Vector2Int Position;
+        public RoomType Room;
+    }
+}
+public class WFC_Script : MonoBehaviour
+{
+    [SerializeField] List<GameObject> RoomPrefabs = new List<GameObject>();
+    [SerializeField] Dictionary<Vector2Int, GameObject> PlacedRooms = new Dictionary<Vector2Int, GameObject>();
+
+    private void PlaceRoom(Tile tile)
+    {
+        GameObject go = Instantiate(RoomPrefabs[(int)tile.Room]);
+        go.transform.position = new Vector3(tile.Position.x, 0, tile.Position.y);
+        PlacedRooms.Add(tile.Position, go);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+}
