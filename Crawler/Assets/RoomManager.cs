26b1861da122de5d291af9413ff8af54d7ea3b75
SzymonKonieczny <kowladMC@gmail.com>
2023-06-02 17:17:58 +0200

    dasdas

--- Crawler/Assets/RoomManager.cs
+++ Crawler/Assets/RoomManager.cs
@@ -9,7 +9,10 @@ public class RoomManager : MonoBehaviour
    // [RequiredInterface(typeof(IRoomPlacer))]
     public WFC_Script _RoomPlacer;
     public IRoomPlacer RoomPlacer;
-        // Start is called before the first frame update
+    // Start is called before the first frame update
+    List<Transform> Rooms;
+    [SerializeField]Transform ChosenRoom;
+    [SerializeField] Transform ChosenExit;
 
     void Start()
     {
@@ -19,8 +22,17 @@ public class RoomManager : MonoBehaviour
     IEnumerator GenerateDungeon()
     {
         yield return StartCoroutine(RoomPlacer.GenerateMap());
+        Rooms = RoomPlacer.GetRoomTransforms();
+        ChosenRoom = Rooms[UnityEngine.Random.Range(0, Rooms.Count)];
+        Rooms.Sort((Transform a, Transform b) =>
+        {
+            float distanceA = Vector3.Distance(a.position, ChosenRoom.transform.position);
+            float distanceB = Vector3.Distance(b.position, ChosenRoom.transform.position);
 
-
+            if (distanceA > distanceB) return 1;
+            else return -1;
+        });
+        ChosenExit = Rooms[Rooms.Count-1];
     }
     // Update is called once per frame
     void Update()
