a5ce48c6d66c87dd6794eeb76370f2e319f0ebdd
SzymonKonieczny <kowladMC@gmail.com>
2023-06-06 19:36:37 +0200

    dsa

--- Crawler/Assets/Scripts/WFC/WFC.cs
+++ Crawler/Assets/Scripts/WFC/WFC.cs
@@ -299,7 +299,7 @@ public class WFC : MonoBehaviour, IRoomPlacer
             FinalWFCMap.Clear();
             WFCMap.Clear();
             PlacedFloors = 0;
-            StartCoroutine(GenerateWFCMap());
+            yield return StartCoroutine(GenerateWFCMap());
             yield break;
         }
 
